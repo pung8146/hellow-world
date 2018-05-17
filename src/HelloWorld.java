da583350fb0f0383163dc41b793f11aa17b5c5a2
pung8 <pung8@DESKTOP-P18AGLT>
2018-05-18 08:29:38 +0900

    thr_world 추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Wolrd");
 		System.out.println("hi world");
+		System.out.println("thr_world");
 	}
 }
